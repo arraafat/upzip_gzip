dea4b4adab2eaa2a2e5499e7620fa2171cf35fc2
Ahmed Raafat <78147641+arraafat@users.noreply.github.com>
2021-05-16 22:19:21 +0100

    Update csv_gzip.py

--- csv_gzip.py
+++ csv_gzip.py
@@ -1,11 +1,16 @@
-def lambda_handler(event, context):
-s3 = boto3.client('s3', use_ssl = False)
+import json
+import csv
+import gzip
+import boto3
+import zipfile
+from io import BytesIO
+#https://medium.com/@johnpaulhayes/how-extract-a-huge-zip-file-in-an-amazon-s3-bucket-by-using-aws-lambda-and-python-e32c6cf58f06
+#https://stackoverflow.com/questions/55018973/unzip-file-from-s3-write-to-a-csv-file-and-push-back-to-s3
 
-s3.upload_fileobj(
-    Fileobj = gzip.GzipFile(
-        None,
-        'rb',
+def lambda_handler(event, context):
+    s3 = boto3.client('s3', use_ssl = False)
+    s3.upload_fileobj(Fileobj = gzip.GzipFile(None,'rb',
         fileobj = BytesIO(
-            s3.get_object(Bucket='bucketName', Key='key')['Body'].read())),
-            Bucket ='bucketName',
-            Key ='key')
+                s3.get_object(Bucket='<Your bucket name >', Key='<Your file prefix>')['Body'].read())),
+                Bucket ='<Your output bucket name>',
+                Key ='Your file prefix.csv')
